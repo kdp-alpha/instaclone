053de8fc69e1d6be160e734178760d69928dda1a
karan-pawar12 <kdpawar752@gmail.com>
2024-09-11 21:55:32 +0530

    updated cb file

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,7 +5,13 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            },
+
+        );
         console.log("MongoDB connected");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
